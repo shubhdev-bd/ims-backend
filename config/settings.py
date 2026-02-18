6e3e01dad5de2865f253a6180e25aa9dea1cfd31
shubhdev-bd <shubham.saxena@believersdestination.com>
2026-02-18 11:30:54 +0000

    ..

--- config/settings.py
+++ config/settings.py
@@ -22,8 +22,8 @@ ALLOWED_HOSTS = [
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "localhost",
-    "127.0.0.1",
+   "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "https://ims-backend-e4fp.onrender.com",
 ]
 
