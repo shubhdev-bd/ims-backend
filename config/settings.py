cecc9db98cc210a9e09f935febf485b440dd543c
shubhdev-bd <shubham.saxena@believersdestination.com>
2026-02-25 07:24:43 +0000

    setting CORS

--- config/settings.py
+++ config/settings.py
@@ -171,7 +171,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 # REST Framework Settings
