2dea896b77344495c4ed938c4ad525b1a0ce9e00
shubhdev-bd <shubham.saxena@believersdestination.com>
2026-02-25 07:17:08 +0000

    ..setting

--- config/settings.py
+++ config/settings.py
@@ -168,6 +168,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "https://ims-backend-e4fp.onrender.com",
     "https://ims-frontend-lilac-alpha.vercel.app",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
